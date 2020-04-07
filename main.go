ca2d1b880de19044798b3eced496dfdca471c17a
Shawn Smith <ShawnSmith0828@gmail.com>
2020-04-06 20:14:53 -0400

    Added int formatting
    Stole the int formatting code from Stackoverflow

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"github.com/aws/aws-lambda-go/lambda" // Needed for Lambda handler
 	"io/ioutil"                           // Needed to read from the web response
 	"net/http"                            // Needed for http calls
-	"os"
+	"os"                                  // Needed to read env variables
+	"strconv"                             // Needed for the int formatting
 )
 
 const DEBUG = false
@@ -37,13 +38,39 @@ func lambdaHandler() {
 		fmt.Println(cases)
 	}
 
-	DiscordMessage := BuildMessage(fmt.Sprintf("```\\nCases: %d\\nDeaths: %d\\nRecovered: %d\\n```", cases.Total, cases.Deaths, cases.Recovered))
+	DiscordMessage := BuildMessage(fmt.Sprintf("```\\nCases: %13s\\nDeaths: %9s\\nRecovered: %7s\\n```", Format(cases.Total), Format(cases.Deaths), Format(cases.Recovered)))
 	PostWebhook(DiscordWebHook, DiscordMessage)
 }
 
 func main() {
 	//lambdaHandler()
 	lambda.Start(lambdaHandler)
+}
+
+// Format ints with commas.
+func Format(n int64) string {
+	in := strconv.FormatInt(n, 10)
+	numOfDigits := len(in)
+	if n < 0 {
+		numOfDigits-- // First character is the - sign (not a digit)
+	}
+	numOfCommas := (numOfDigits - 1) / 3
+
+	out := make([]byte, len(in)+numOfCommas)
+	if n < 0 {
+		in, out[0] = in[1:], '-'
+	}
+
+	for i, j, k := len(in)-1, len(out)-1, 0; ; i, j = i-1, j-1 {
+		out[j] = in[i]
+		if i == 0 {
+			return string(out)
+		}
+		if k++; k == 3 {
+			j, k = j-1, 0
+			out[j] = ','
+		}
+	}
 }
 
 func BuildMessage(message string) []byte {
